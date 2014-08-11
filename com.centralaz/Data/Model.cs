33c56efe5eeca45ab958283dcaa70a3259bdef55
nairdo <nairdo@gmail.com>
2014-08-11 10:36:10 -0700

    remove old com.centralaz project.

--- com.centralaz/Data/Model.cs
+++
@@ -1,9 +0,0 @@
-using System.Runtime.Serialization;
-
-namespace com.centralaz.Data
-{
-    [DataContract]
-    public class Model<T> : Rock.Data.Model<T> where T : Rock.Data.Model<T>, Rock.Security.ISecured, new()
-    {
-    }
-}
